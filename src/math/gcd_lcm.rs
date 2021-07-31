6a5bdc7fd82083032033b3f6597a1487b876f59a
Haar <haar@haar-pc>
2021-08-01 08:46:47 +0900

    [new] gcd_lcm

---
+++ src/math/gcd_lcm.rs
@@ -0,0 +1,50 @@
+pub trait GcdLcm {
+    type Output;
+
+    fn gcd(&self, _: Self::Output) -> Self::Output;
+    fn lcm(&self, _: Self::Output) -> Self::Output;
+    fn gcd_lcm(&self, _: Self::Output) -> (Self::Output, Self::Output);
+}
+
+macro_rules! gcd_lcm_impl_all {
+    ( $($t:ty),* ) => {
+        $(
+            impl GcdLcm for $t {
+                type Output = $t;
+                fn gcd(&self, mut b: Self::Output) -> Self::Output {
+                    let mut a = *self;
+
+                    if a < b {
+                        std::mem::swap(&mut a, &mut b);
+                    }
+
+                    if b == 0 {
+                        return a;
+                    }
+
+                    b.gcd(a % b)
+                }
+
+                fn lcm(&self, b: Self::Output) -> Self::Output {
+                    self / self.gcd(b) * b
+                }
+
+                fn gcd_lcm(&self, b: Self::Output) -> (Self::Output, Self::Output) {
+                    (self.gcd(b), self.lcm(b))
+                }
+            }
+        )*
+    }
+}
+
+gcd_lcm_impl_all! (i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!((12 as i32).gcd_lcm(8), (4, 24));
+    }
+}
