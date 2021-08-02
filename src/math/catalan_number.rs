b959c99f755f4905a80efa8067782ae8aded6f29
Haar <haar@haar-pc>
2021-08-03 05:31:46 +0900

    [new] catalan_number

---
+++ src/math/catalan_number.rs
@@ -0,0 +1,47 @@
+use std::ops::{ Mul, Div, Sub };
+use crate::math::factorial::{ FactorialTable };
+
+impl<T> FactorialTable<T>
+where
+    T: Copy + Mul<Output = T> + Div<Output = T> + Sub<Output = T> + From<usize>
+{
+    pub fn catalan_number(&self, n: usize) -> T {
+        if n == 0 {
+            T::from(1)
+        }
+        else {
+            self.comb(2 * n, n) - self.comb(2 * n, n - 1)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::math::modint::{ ModInt };
+    use crate::misc::generics_int::{ GenericsInt };
+    use crate::generics_int;
+
+    generics_int!(G1000000007, 1000000007);
+    type Mint = ModInt<G1000000007>;
+
+    #[test]
+    fn test() {
+        let ft = FactorialTable::<Mint>::new(100);
+
+        let catalans = (0 ..= 30).map(|i| ft.catalan_number(i)).collect::<Vec<_>>();
+
+        // https://oeis.org/A000108/list
+        let ans: Vec<u64> = vec![
+            1,1,2,5,14,42,132,429,1430,4862,16796,58786,
+            208012,742900,2674440,9694845,35357670,129644790,
+            477638700,1767263190,6564120420,24466267020,
+            91482563640,343059613650,1289904147324,
+            4861946401452,18367353072152,69533550916004,
+            263747951750360,1002242216651368,3814986502092304
+        ];
+        let ans = ans.into_iter().map(|x| Mint::from(x)).collect::<Vec<_>>();
+
+        assert_eq!(catalans, ans);
+    }
+}
