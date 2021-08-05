2fcbf5db118cf2b0f8afe05eda2fceabfa38a8ca
Haar <haar@haar-pc>
2021-08-06 07:46:14 +0900

    [upd] rle

--- src/seq/run_length_encoding.rs
+++ src/seq/run_length_encoding.rs
@@ -1,5 +1,4 @@
-
-fn run_length_encoding<T>(a: &[T]) -> Vec<(T, usize)>
+pub fn run_length_encoding<T>(a: &[T]) -> Vec<(T, usize)>
 where
     T: Eq + Clone
 {
@@ -22,10 +21,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test() {
+    fn test_vec() {
         let a = vec![1, 1, 2, 1, 3, 4, 5, 5, 4];
         assert_eq!(run_length_encoding(&a), [(1, 2), (2, 1), (1, 1), (3, 1), (4, 1), (5, 2), (4, 1)]);
+    }
 
+    #[test]
+    fn test_str() {
         let a = "aabbccd";
         assert_eq!(run_length_encoding(&a.chars().collect::<Vec<_>>()), [('a', 2), ('b', 2), ('c', 2), ('d', 1)]);
     }
