3a15d27679348b2a82d0a2ff66fd22b3aa89392c
Haar <haar@haar-pc>
2021-08-06 18:46:45 +0900

    [new] sliding_min

--- src/algorithm/mod.rs
+++ src/algorithm/mod.rs
@@ -1,3 +1,4 @@
 pub mod cumulative_sum_1d;
 pub mod subset_sum;
 pub mod inversion_number;
+pub mod sliding_window;
---
+++ src/algorithm/sliding_window.rs
@@ -0,0 +1,44 @@
+use std::collections::VecDeque;
+
+/// 配列のすべての長さkの連続部分列について、その最小値を列挙する。
+///
+/// Time complexity O(n)
+pub fn sliding_minimum<T: Ord + Clone>(a: &[T], k: usize) -> Vec<T> {
+    let n = a.len();
+
+    let mut dq = VecDeque::new();
+    let mut ret = Vec::new();
+
+    for i in 0 .. k {
+        while !dq.is_empty() && a[*dq.back().unwrap()] >= a[i] {
+            dq.pop_back();
+        }
+        dq.push_back(i);
+    }
+
+    for i in 0 .. n - k + 1 {
+        while *dq.front().unwrap() < i {
+            dq.pop_front();
+        }
+
+        ret.push(a[*dq.front().unwrap()].clone());
+
+        while !dq.is_empty() && i + k < n && a[*dq.back().unwrap()] >= a[i + k] {
+            dq.pop_back();
+        }
+
+        dq.push_back(i + k);
+    }
+
+    ret
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(sliding_minimum(&[1, 7, 7, 4, 8, 1, 6], 3), [1, 4, 4, 1, 1]);
+    }
+}
