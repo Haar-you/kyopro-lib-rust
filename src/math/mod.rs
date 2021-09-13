eade7264af148c7f45a902852bd0a9d6a1e0b2e5
Haar <haar@haar-pc>
2021-09-13 15:27:00 +0900

    [new] primitive root

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -4,13 +4,15 @@ pub mod ext_gcd;
 pub mod factorize;
 pub mod gcd_lcm;
 pub mod mod_ops;
-pub mod modint;
 pub mod nim_product;
 pub mod prime;
+pub mod primitive_root;
+
+pub mod ff_traits;
+pub mod modint;
 
 pub mod bell_number;
 pub mod binomial_coefficient;
-pub mod catalan_number;
 pub mod factorial;
 
 pub mod convolution;
---
+++ src/math/primitive_root.rs
@@ -0,0 +1,26 @@
+use crate::math::{mod_ops::pow::*, factorize::trial::*};
+
+pub fn primitive_root(p: u64) -> Option<u64> {
+    let pf = factorize(p - 1).into_iter().map(|(x, _)| x).collect::<Vec<_>>();
+
+    for g in 2..=p {
+        if pf.iter().all(|f| mod_pow(g, (p - 1) / f, p) != 1) {
+            return Some(g);
+        }
+    }
+
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(primitive_root(469762049), Some(3));
+        assert_eq!(primitive_root(167772161), Some(3));
+        assert_eq!(primitive_root(754974721), Some(11));
+        assert_eq!(primitive_root(1012924417), Some(5));
+    }
+}
