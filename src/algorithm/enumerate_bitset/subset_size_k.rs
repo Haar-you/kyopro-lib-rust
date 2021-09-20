2a14b2a70c8abbc3102b8d89ef8e0574493129a7
Haar <haar@haar-pc>
2021-09-21 03:16:02 +0900

    subset_size_k

--- src/algorithm/enumerate_bitset/subset_size_k.rs
+++ src/algorithm/enumerate_bitset/subset_size_k.rs
@@ -1,28 +1,18 @@
-pub struct SubsetSizeK {
-    n: u32,
-    c: u32,
-}
+use std::iter::successors;
 
-impl Iterator for SubsetSizeK {
-    type Item = u32;
+pub fn subset_size_k(n: u32, k: u32) -> impl Iterator<Item = u32> {
+    let c = (1 << k) - 1;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.c >= 1 << self.n {
+    successors(Some(c), move |&t| {
+        let x = ((t as i32) & (-(t as i32))) as u32;
+        let y = t + x;
+        let t = ((t & !y) / x) >> 1 | y;
+        if t >= 1 << n {
             None
         } else {
-            let ret = self.c;
-
-            let x = ((self.c as i32) & (-(self.c as i32))) as u32;
-            let y = self.c + x;
-            self.c = ((self.c & !y) / x) >> 1 | y;
-
-            Some(ret)
+            Some(t)
         }
-    }
-}
-
-pub fn subset_size_k(n: u32, k: u32) -> SubsetSizeK {
-    SubsetSizeK { n, c: (1 << k) - 1 }
+    })
 }
 
 #[cfg(test)]
