36f8962f2052815b33a4f0c675dec8b1d0b6135b
Haar <haar@haar-pc>
2021-10-28 12:01:13 +0900

    split subset_sum mod

--- src/algorithm/subset_sum/limited.rs
+++ src/algorithm/subset_sum/limited.rs
@@ -1,35 +1,3 @@
-use std::ops::Add;
-
-/// 部分和問題
-///
-/// Time complexity O(nk)
-///
-/// Space complexity O(k)
-pub fn subset_sum<T>(n: usize, k: usize, a: &[usize]) -> Vec<T>
-where
-    T: Copy + Clone + From<usize> + Add<Output = T>,
-{
-    assert!(a.len() == n);
-
-    let mut dp = vec![vec![T::from(0); k + 1]; 2];
-
-    dp[0][0] = T::from(1);
-
-    for (i, &x) in a.iter().enumerate() {
-        let cur = i & 1;
-        let next = (i + 1) & 1;
-        for j in 0..=k {
-            if j >= x {
-                dp[next][j] = dp[cur][j - x] + dp[cur][j];
-            } else {
-                dp[next][j] = dp[cur][j];
-            }
-        }
-    }
-
-    dp[n & 1].clone()
-}
-
 /// 各要素に最大使用回数が定められている部分和問題
 ///
 /// Time complexity O(nk)
