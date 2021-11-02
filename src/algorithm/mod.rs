7b64af301bfcb9509cc3cd5cd1d52abf02b6a339
Haar <haar@haar-pc>
2021-11-02 17:03:53 +0900

    [new] 2sat

--- src/algorithm/mod.rs
+++ src/algorithm/mod.rs
@@ -1,6 +1,7 @@
 pub mod bsearch;
 pub mod compressor;
 pub mod enumerate_bitset;
+pub mod imos_1d;
 pub mod interval_scheduling;
 pub mod inversion_number;
 pub mod knapsack;
@@ -12,3 +13,4 @@ pub mod mo;
 pub mod permutation;
 pub mod sliding_window;
 pub mod subset_sum;
+pub mod two_sat;
---
+++ src/algorithm/two_sat.rs
@@ -0,0 +1,51 @@
+use crate::graph::{scc::*, template::*};
+
+pub struct TwoSat {
+    size: usize,
+    g: Graph<u8>,
+}
+
+impl TwoSat {
+    pub fn new(size: usize) -> Self {
+        Self {
+            size,
+            g: Graph::new(2 * size),
+        }
+    }
+
+    fn check(&self, x: isize) -> usize {
+        assert!(x != 0);
+        assert!(x.abs() as usize <= self.size);
+
+        if x > 0 {
+            x as usize - 1
+        } else {
+            x.abs() as usize + self.size - 1
+        }
+    }
+
+    pub fn add_if(&mut self, a: isize, b: isize) {
+        self.g.add_directed(self.check(a), self.check(b), 1);
+    }
+
+    pub fn add_or(&mut self, a: isize, b: isize) {
+        self.add_if(-a, b);
+        self.add_if(-b, a);
+    }
+
+    pub fn not_coexist(&mut self, a: isize, b: isize) {
+        self.add_or(-a, -b);
+    }
+
+    pub fn solve(&self) -> Option<Vec<bool>> {
+        let (s, _) = scc(&self.g);
+
+        for i in 0..self.size {
+            if s[i] == s[i + self.size] {
+                return None;
+            }
+        }
+
+        Some((0..self.size).map(|i| s[i] > s[i + self.size]).collect())
+    }
+}
