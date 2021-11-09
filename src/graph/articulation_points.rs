019c7f87e5b012f071012bec08f82fe6e71ad2eb
Haar <haar@haar-pc>
2021-11-10 04:35:25 +0900

    [new] lowlink

--- src/graph/articulation_points.rs
+++ src/graph/articulation_points.rs
@@ -1,61 +1,16 @@
-use crate::graph::template::*;
-use std::cmp::min;
+use crate::graph::{lowlink::*, *};
 
 pub fn articulation_points<T>(g: &Graph<T>) -> Vec<usize> {
     let n = g.len();
-    let mut visit = vec![None; n];
-    let mut low = vec![0; n];
-    let mut ret = vec![];
-    let mut v = 0;
 
-    for i in 0..n {
-        if visit[i].is_none() {
-            articulation_points_(g, i, i, &mut visit, &mut low, &mut ret, &mut v);
-        }
-    }
+    let Lowlink { ord, low, par, ch } = Lowlink::new(&g);
 
-    ret
-}
-
-fn articulation_points_<T>(
-    g: &Graph<T>,
-    root: usize,
-    cur: usize,
-    visit: &mut Vec<Option<usize>>,
-    low: &mut Vec<usize>,
-    ret: &mut Vec<usize>,
-    v: &mut usize,
-) -> usize {
-    if let Some(x) = visit[cur] {
-        return x;
-    }
-
-    visit[cur] = Some(*v);
-
-    let mut temp = *v;
-    let mut children = vec![];
-    *v += 1;
-
-    for &Edge { to, .. } in &g.edges[cur] {
-        if visit[to].is_none() {
-            children.push(to);
-        }
-        let t = articulation_points_(g, root, to, visit, low, ret, v);
-        temp = min(temp, t);
-    }
-
-    low[cur] = temp;
-
-    if cur != root || children.len() >= 2 {
-        for x in children {
-            if low[x] >= visit[cur].unwrap() {
-                ret.push(cur);
-                break;
-            }
-        }
-    }
-
-    low[cur]
+    (0..n)
+        .filter(|&i| {
+            (par[i].is_none() && ch[i].len() >= 2)
+                || ch[i].iter().any(|&j| par[i].is_some() && ord[i] <= low[j])
+        })
+        .collect()
 }
 
 #[cfg(test)]
---
+++ src/graph/lowlink.rs
@@ -0,0 +1,62 @@
+use crate::graph::*;
+use std::cmp::min;
+
+pub struct Lowlink {
+    pub ord: Vec<usize>,
+    pub low: Vec<usize>,
+    pub par: Vec<Option<usize>>,
+    pub ch: Vec<Vec<usize>>,
+}
+
+impl Lowlink {
+    pub fn new<T>(g: &Graph<T>) -> Self {
+        let n = g.len();
+        let mut ret = Self {
+            ord: vec![0; n],
+            low: vec![0; n],
+            par: vec![None; n],
+            ch: vec![vec![]; n],
+        };
+
+        let mut index = 0;
+        let mut check = vec![false; n];
+        for i in 0..n {
+            ret.dfs(&g, i, None, &mut index, &mut check);
+        }
+
+        ret
+    }
+
+    fn dfs<T>(
+        &mut self,
+        g: &Graph<T>,
+        cur: usize,
+        par: Option<usize>,
+        index: &mut usize,
+        check: &mut [bool],
+    ) {
+        if check[cur] {
+            return;
+        }
+        check[cur] = true;
+
+        self.par[cur] = par;
+        self.ord[cur] = *index;
+        self.low[cur] = *index;
+        *index += 1;
+
+        for &Edge { to, .. } in &g.edges[cur] {
+            if par.map_or(false, |p| p == cur) {
+                continue;
+            }
+
+            if !check[to] {
+                self.ch[cur].push(to);
+                self.dfs(g, to, Some(cur), index, check);
+                self.low[cur] = min(self.low[cur], self.low[to]);
+            }
+
+            self.low[cur] = min(self.low[cur], self.ord[to]);
+        }
+    }
+}
