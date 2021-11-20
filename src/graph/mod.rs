22978cf59d34a3f0f6cafa125ef89ae1d6c95c70
Haar <haar@haar-pc>
2021-11-21 06:11:48 +0900

    [upd] chinese_postman

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -2,7 +2,7 @@ pub mod articulation_points;
 // pub mod bellman_ford;
 // pub mod bipartite;
 pub mod bridges;
-// pub mod chinese_postman;
+pub mod chinese_postman;
 pub mod dijkstra;
 pub mod kruskal;
 pub mod lowlink;
