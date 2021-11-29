f1cd42eb465807fcb84ec0a6eaa5121b17824013
Haar-you <zemlya.pole@gmail.com>
2021-11-29 16:33:50 +0900

    [upd] compressor

--- src/algo/compressor.rs
+++ src/algo/compressor.rs
@@ -1,3 +1,4 @@
+//! 座標圧縮
 use crate::algo::bsearch::lower_bound;
 
 #[derive(Clone)]
@@ -14,19 +15,25 @@ impl<T> Compressor<T>
 where
     T: Clone + Ord + Eq,
 {
+    /// `value`が何番目の値(0-index)かを返す。
+    /// # Complexity
+    /// Time complexity O(log(n))
     pub fn index(&self, value: &T) -> usize {
         lower_bound(&self.data, value)
     }
 
-    pub fn get(&self, i: usize) -> T {
-        self.data[i].clone()
+    /// `i`番目の値を返す。
+    /// # Complexity
+    /// Time complexity O(1)
+    pub fn get(&self, i: usize) -> &T {
+        &self.data[i]
     }
 
-    pub fn compress(&self, values: Vec<T>) -> Vec<usize> {
+    pub fn compress(&self, values: impl IntoIterator<Item = T>) -> Vec<usize> {
         values.into_iter().map(|x| self.index(&x)).collect()
     }
 
-    pub fn decompress(&self, indices: Vec<usize>) -> Vec<T> {
+    pub fn decompress(&self, indices: impl IntoIterator<Item = usize>) -> Vec<&T> {
         indices.into_iter().map(|i| self.get(i)).collect()
     }
 
@@ -44,8 +51,8 @@ impl<T: Clone + Ord + Eq> CompressorBuilder<T> {
         self.data.push(value);
     }
 
-    pub fn add_vec(&mut self, values: Vec<T>) {
-        self.data.extend(values);
+    pub fn add_vec(&mut self, values: impl IntoIterator<Item = T>) {
+        self.data.extend(values.into_iter());
     }
 
     pub fn build(mut self) -> Compressor<T> {
@@ -58,6 +65,8 @@ impl<T: Clone + Ord + Eq> CompressorBuilder<T> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::hashset;
+    use std::collections::HashSet;
 
     #[test]
     fn test() {
@@ -70,6 +79,34 @@ mod tests {
             compressor.compress(data.clone()),
             vec![2, 4, 3, 5, 6, 7, 1, 0, 4]
         );
-        assert_eq!(compressor.decompress(vec![2, 4, 3, 5, 6, 7, 1, 0, 4]), data);
+        assert_eq!(
+            compressor
+                .decompress(vec![2, 4, 3, 5, 6, 7, 1, 0, 4])
+                .into_iter()
+                .copied()
+                .collect::<Vec<_>>(),
+            data
+        );
+
+        let data = hashset![1, 3, 2, 4, 5, 9, 0, -1, 3];
+        let mut builder = CompressorBuilder::<_>::new();
+        builder.add_vec(data.clone());
+        let compressor = builder.build();
+
+        assert_eq!(
+            compressor
+                .compress(data.clone())
+                .into_iter()
+                .collect::<HashSet<_>>(),
+            hashset![2, 4, 3, 5, 6, 7, 1, 0, 4]
+        );
+        assert_eq!(
+            compressor
+                .decompress(vec![2, 4, 3, 5, 6, 7, 1, 0, 4])
+                .into_iter()
+                .copied()
+                .collect::<HashSet<_>>(),
+            data
+        );
     }
 }
