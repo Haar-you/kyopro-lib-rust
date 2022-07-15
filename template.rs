d3f95e435ae57333f3aed4b08c8ec912e084c1fc
Haar-you <zemlya.pole@gmail.com>
2022-07-15 15:51:24 +0900

    template.rs

---
+++ template.rs
@@ -0,0 +1,28 @@
+pub mod main {
+    use super::*;
+    use haar_lib::{
+        get,
+        input,
+        io,
+        //chmin, chmax,
+        //mul_vec,
+        utils::join_str::*,
+    };
+
+    use std::io::{Read, Write};
+
+    #[derive(Clone, Default)]
+    pub struct Problem {/* write variables here */}
+
+    impl Problem {
+        pub fn main(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+
+            Ok(())
+        }
+        /* write functions here */
+    }
+}
+
+fn main() {
+    main::Problem::default().main().unwrap();
+}
