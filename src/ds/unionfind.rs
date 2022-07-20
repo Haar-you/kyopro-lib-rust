b53d4453b9b06320267d30b4d4a4af5c6dfef9d9
Haar-you <zemlya.pole@gmail.com>
2022-07-20 15:56:46 +0900

    [update] unionfind

--- src/ds/unionfind.rs
+++ src/ds/unionfind.rs
@@ -1,7 +1,9 @@
+use std::cell::Cell;
+
 pub struct UnionFind {
     n: usize,
     count: usize,
-    parent: Vec<usize>,
+    parent: Vec<Cell<usize>>,
     depth: Vec<usize>,
     size: Vec<usize>,
 }
@@ -11,21 +13,22 @@ impl UnionFind {
         UnionFind {
             n,
             count: n,
-            parent: (0..n).collect(),
+            parent: (0..n).map(Cell::new).collect(),
             depth: vec![1; n],
             size: vec![1; n],
         }
     }
 
-    pub fn root_of(&mut self, i: usize) -> usize {
-        if self.parent[i] == i {
+    pub fn root_of(&self, i: usize) -> usize {
+        if self.parent[i].get() == i {
             return i;
         }
-        self.parent[i] = self.root_of(self.parent[i]);
-        self.parent[i]
+        let p = self.parent[i].get();
+        self.parent[i].set(self.root_of(p));
+        self.parent[i].get()
     }
 
-    pub fn is_same(&mut self, i: usize, j: usize) -> bool {
+    pub fn is_same(&self, i: usize, j: usize) -> bool {
         self.root_of(i) == self.root_of(j)
     }
 
@@ -40,11 +43,11 @@ impl UnionFind {
         self.count -= 1;
 
         if self.depth[i] < self.depth[j] {
-            self.parent[i] = j;
+            self.parent[i].set(j);
             self.size[j] += self.size[i];
             j
         } else {
-            self.parent[j] = i;
+            self.parent[j].set(i);
             self.size[i] += self.size[j];
             if self.depth[i] == self.depth[j] {
                 self.depth[i] += 1;
@@ -53,7 +56,7 @@ impl UnionFind {
         }
     }
 
-    pub fn size_of(&mut self, i: usize) -> usize {
+    pub fn size_of(&self, i: usize) -> usize {
         let i = self.root_of(i);
         self.size[i]
     }
@@ -62,7 +65,7 @@ impl UnionFind {
         self.count
     }
 
-    pub fn get_groups(&mut self) -> Vec<Vec<usize>> {
+    pub fn get_groups(&self) -> Vec<Vec<usize>> {
         let mut ret = vec![vec![]; self.n];
 
         for i in 0..self.n {
@@ -74,4 +77,55 @@ impl UnionFind {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+    use crate::btreeset;
+    use rand::Rng;
+    use std::collections::BTreeSet;
+    use std::iter::FromIterator;
+
+    #[test]
+    fn test() {
+        let n = 100;
+        let q = 50;
+        let mut rng = rand::thread_rng();
+
+        let mut uf = UnionFind::new(n);
+        let mut a = (0..n).map(|i| btreeset![i]).collect::<BTreeSet<_>>();
+
+        for _ in 0..q {
+            let i = rng.gen::<usize>() % n;
+            let j = rng.gen::<usize>() % n;
+
+            uf.merge(i, j);
+
+            let mut ai = a.iter().find(|s| s.contains(&i)).unwrap().clone();
+            let aj = a.iter().find(|s| s.contains(&j)).unwrap().clone();
+
+            if ai != aj {
+                a.remove(&ai);
+                a.remove(&aj);
+                ai.extend(aj);
+                a.insert(ai);
+            }
+        }
+
+        for _ in 0..q {
+            let i = rng.gen::<usize>() % n;
+            let j = rng.gen::<usize>() % n;
+
+            let ai = a.iter().find(|s| s.contains(&i)).unwrap();
+
+            assert_eq!(uf.is_same(i, j), ai.contains(&j));
+        }
+
+        assert_eq!(
+            BTreeSet::from_iter(
+                uf.get_groups()
+                    .into_iter()
+                    .map(|s| BTreeSet::from_iter(s.into_iter()))
+            ),
+            a
+        );
+    }
+}
