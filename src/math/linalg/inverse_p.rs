5d8092b34c430d6fbb2a408327c10c4a53cbbfcb
Haar-you <zemlya.pole@gmail.com>
2022-08-13 16:28:54 +0900

    [new] inverse matrix mod p

---
+++ src/math/linalg/inverse_p.rs
@@ -0,0 +1,77 @@
+use crate::math::mod_ops::inv_p::mod_inv_p;
+
+/// 素数mod p上での逆行列を求める。
+/// $O(n^3)$
+#[inline]
+pub fn inverse_p(mut b: Vec<Vec<u64>>, p: u64) -> Option<Vec<Vec<u64>>> {
+    let n = b.len();
+
+    assert!(b.iter().all(|r| r.len() == n));
+
+    for (i, bi) in b.iter_mut().enumerate() {
+        bi.resize(2 * n, 0);
+        bi[i + n] = 1;
+    }
+
+    for i in 0..n {
+        let q = (i..n).find(|&j| b[j][i] != 0)?;
+
+        b.swap(i, q);
+
+        let d = mod_inv_p(b[i][i], p);
+
+        for x in b[i].iter_mut() {
+            *x = *x * d % p;
+        }
+
+        let d = mod_inv_p(b[i][i], p);
+
+        let bi = b.swap_remove(i);
+
+        for bj in b.iter_mut() {
+            let d = bj[i] * d % p;
+
+            for (x, y) in bj.iter_mut().zip(bi.iter()) {
+                let t = *y * d % p;
+                if *x >= t {
+                    *x -= t;
+                } else {
+                    *x = p - t + *x;
+                }
+            }
+        }
+
+        b.push(bi);
+        b.swap(i, n - 1);
+    }
+
+    let ret = b.into_iter().map(|a| a[n..].to_vec()).collect();
+
+    Some(ret)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let p = 998244353;
+
+        let a = vec![vec![3, 1, 4], vec![1, 5, 9], vec![2, 6, 5]];
+        assert_eq!(
+            inverse_p(a, p),
+            Some(vec![
+                vec![188557267, 255106890, 587855008],
+                vec![122007643, 987152749, 321656514],
+                vec![576763404, 310564910, 976061145]
+            ])
+        );
+
+        let a = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+        assert_eq!(inverse_p(a, p), None);
+
+        let a = vec![vec![0, 1], vec![1, 0]];
+        assert_eq!(inverse_p(a, p), Some(vec![vec![0, 1], vec![1, 0]]))
+    }
+}
--- src/math/linalg/mod.rs
+++ src/math/linalg/mod.rs
@@ -1,2 +1,4 @@
 pub mod matrix;
 pub mod square_matrix;
+
+pub mod inverse_p;
