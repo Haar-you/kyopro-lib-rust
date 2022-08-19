b4366dc29fbf768fa9ed6f938e4f4901df9fd892
Haar-you <zemlya.pole@gmail.com>
2022-08-19 18:58:59 +0900

    [add] test

--- src/algo/bsearch.rs
+++ src/algo/bsearch.rs
@@ -26,6 +26,7 @@ macro_rules! bsearch_impl {
 ///
 /// # Complexity
 /// Time complexity $O(\log(n))$
+#[inline]
 pub fn lower_bound<T: Ord>(a: &[T], value: &T) -> usize {
     bsearch_impl!(<, a, value)
 }
@@ -34,6 +35,7 @@ pub fn lower_bound<T: Ord>(a: &[T], value: &T) -> usize {
 ///
 /// # Complexity
 /// Time complexity $O(\log(n))$
+#[inline]
 pub fn upper_bound<T: Ord>(a: &[T], value: &T) -> usize {
     bsearch_impl!(<=, a, value)
 }
@@ -42,6 +44,7 @@ pub fn upper_bound<T: Ord>(a: &[T], value: &T) -> usize {
 ///
 /// # Complexity
 /// Time complexity $O(\log(n))$
+#[inline]
 pub fn equal_range<T: Ord>(a: &[T], value: &T) -> (usize, usize) {
     (lower_bound(a, value), upper_bound(a, value))
 }
@@ -49,6 +52,7 @@ pub fn equal_range<T: Ord>(a: &[T], value: &T) -> (usize, usize) {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rand::Rng;
 
     #[test]
     fn test_lower_bound() {
@@ -77,5 +81,39 @@ mod tests {
         assert_eq!(equal_range(&a, &5), (4, 7));
         assert_eq!(equal_range(&a, &4), (3, 4));
         assert_eq!(equal_range(&a, &6), (7, 7));
+    }
+
+    #[test]
+    fn test() {
+        let mut rng = rand::thread_rng();
+
+        let n = 100;
+        let a = (0..n)
+            .map(|_| rng.gen_range(0..=10))
+            .scan(0, |state, x| {
+                *state += x;
+                Some(*state)
+            })
+            .collect::<Vec<_>>();
+
+        for x in 0..=1000 {
+            assert_eq!(
+                lower_bound(&a, &x),
+                a.iter()
+                    .enumerate()
+                    .find(|(_, &y)| y >= x)
+                    .map(|(i, _)| i)
+                    .unwrap_or(n)
+            );
+
+            assert_eq!(
+                upper_bound(&a, &x),
+                a.iter()
+                    .enumerate()
+                    .find(|(_, &y)| y > x)
+                    .map(|(i, _)| i)
+                    .unwrap_or(n)
+            );
+        }
     }
 }
