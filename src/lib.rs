0de12fcac2553e13f860b1dd231cf1add62eefce
Haar-you <zemlya.pole@gmail.com>
2022-09-04 05:59:17 +0900

    [update] docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+/*!
+[![Rust](https://github.com/Haar-you/kyopro-lib-rust/actions/workflows/rust.yml/badge.svg)](https://github.com/Haar-you/kyopro-lib-rust/actions/workflows/rust.yml)
+![GitHub last commit](https://img.shields.io/github/last-commit/Haar-you/kyopro-lib-rust)
+
+[Repository](https://github.com/Haar-you/kyopro-lib-rust)
+
+
+
+
+
+*/
+
 pub mod algebra;
 pub mod algo;
 pub mod ds;
