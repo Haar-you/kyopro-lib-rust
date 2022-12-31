1fb3525ca6818c46b6c4103c2ebd107e8600d941
Haar <haar@haar-pc>
2022-12-31 16:55:52 +0900

    [new] parenthesis

---
+++ src/misc/paren.rs
@@ -0,0 +1,28 @@
+/// 括弧列が対応が取れているかを調べる
+///
+/// # Complexity
+/// Time Complexity $O(N)$
+
+pub fn check_paren<T: Copy + Eq>(s: impl IntoIterator<Item = T>, open: T, close: T) -> bool {
+    let mut stack = vec![];
+
+    for c in s {
+        if let Some(_) = stack.last() {
+            if c == close {
+                stack.pop();
+            } else if c == open {
+                stack.push(c);
+            } else {
+                return false;
+            }
+        } else {
+            if c == open {
+                stack.push(c);
+            } else {
+                return false;
+            }
+        }
+    }
+
+    stack.is_empty()
+}
