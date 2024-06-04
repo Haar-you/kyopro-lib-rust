a0255b782fe4731d67331d0c78a6777011142b8d
Haar-you <zemlya.pole@gmail.com>
2024-06-04 18:37:27 +0900

    [update] directed eulerian trail

--- src/graph/eulerian/directed.rs
+++ src/graph/eulerian/directed.rs
@@ -1,13 +1,15 @@
+use crate::graph::*;
+
 #[derive(Clone)]
-pub struct DirectedEulerianTrail {
+pub struct DirectedEulerianTrail<E: EdgeTrait> {
     size: usize,
     edge_count: usize,
-    graph: Vec<Vec<u32>>,
+    graph: Vec<Vec<E>>,
     indeg: Vec<i32>,
     outdeg: Vec<i32>,
 }
 
-impl DirectedEulerianTrail {
+impl<E: EdgeTrait + Clone> DirectedEulerianTrail<E> {
     pub fn new(size: usize) -> Self {
         Self {
             size,
@@ -18,53 +20,59 @@ impl DirectedEulerianTrail {
         }
     }
 
-    pub fn add_edge(&mut self, from: usize, to: usize) {
-        self.graph[from].push(to as u32);
-        self.indeg[to] += 1;
-        self.outdeg[from] += 1;
+    pub fn add_edge(&mut self, e: E) {
+        self.indeg[e.to()] += 1;
+        self.outdeg[e.from()] += 1;
+        self.graph[e.from()].push(e);
         self.edge_count += 1;
     }
 
-    fn dfs(&mut self, cur: usize, trail: &mut Vec<usize>) {
-        if let Some(next) = self.graph[cur].pop() {
-            self.dfs(next as usize, trail);
+    fn dfs(&mut self, cur: usize, vs: &mut Vec<usize>, es: &mut Vec<E>) {
+        while let Some(e) = self.graph[cur].pop() {
+            let next = e.to();
+            self.dfs(next as usize, vs, es);
+            es.push(e);
         }
 
-        while let Some(next) = self.graph[cur].pop() {
-            let mut temp = vec![];
-            self.dfs(next as usize, &mut temp);
-            trail.extend(temp);
-        }
-
-        trail.push(cur);
+        vs.push(cur);
     }
 
-    pub fn solve(mut self) -> Option<Vec<usize>> {
+    /// 有向グラフがオイラー路を持つかを判定する。
+    /// 存在する場合、オイラー路の頂点列と辺列を返す。
+    pub fn solve(mut self) -> Option<(Vec<usize>, Vec<E>)> {
         let mut in_count = 0;
         let mut out_count = 0;
-        let mut start = 0;
+        let mut start = None;
 
         for i in 0..self.size {
             match self.outdeg[i] - self.indeg[i] {
                 0 => {}
                 1 => {
                     out_count += 1;
-                    start = i;
+                    start = Some(i);
                 }
                 -1 => in_count += 1,
                 _ => return None,
             }
+            if start.is_none() && !self.graph[i].is_empty() {
+                start = Some(i);
+            }
         }
+
+        let start = start.unwrap_or(0);
 
         if !(in_count == 0 && out_count == 0 || in_count == 1 && out_count == 1) {
             return None;
         }
 
-        let mut ret = vec![];
-        self.dfs(start, &mut ret);
-        if ret.len() == self.edge_count + 1 {
-            ret.reverse();
-            Some(ret)
+        let mut vs = vec![];
+        let mut es = vec![];
+        self.dfs(start, &mut vs, &mut es);
+        if vs.len() == self.edge_count + 1 && es.len() == self.edge_count {
+            vs.reverse();
+            es.reverse();
+
+            Some((vs, es))
         } else {
             None
         }
