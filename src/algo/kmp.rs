73b0d2aad4b7586290ad10a62775d4ed9b49894b
Haar-you <zemlya.pole@gmail.com>
2024-08-06 05:52:26 +0900

    [update] kmp

--- src/algo/kmp.rs
+++ src/algo/kmp.rs
@@ -1,17 +1,19 @@
+//! Knuth-Morris-Pratt法
+
 #[derive(Clone, Debug)]
-pub struct KMP {
-    pat: Vec<u8>,
+pub struct KMP<T> {
+    pat: Vec<Option<T>>,
     table: Vec<isize>,
 }
 
-impl KMP {
-    pub fn new(pat: &str) -> Self {
+impl<T: PartialEq + Clone> KMP<T> {
+    pub fn new(pat: Vec<T>) -> Self {
         let m = pat.len();
         let mut table: Vec<isize> = vec![0; m + 1];
         table[0] = -1;
 
-        let mut pat: Vec<u8> = pat.as_bytes().to_vec();
-        pat.push(0);
+        let mut pat: Vec<_> = pat.into_iter().map(|a| Some(a)).collect();
+        pat.push(None);
 
         let mut i: usize = 2;
         let mut j: usize = 0;
@@ -31,16 +33,14 @@ impl KMP {
         Self { pat, table }
     }
 
-    pub fn matches(&self, s: &str) -> Vec<usize> {
+    pub fn matches(&self, s: &[T]) -> Vec<usize> {
         let mut ret = vec![];
         let n = s.len();
-
-        let s = s.as_bytes().to_vec();
 
         let mut m = 0;
         let mut i = 0;
         while m + i < n {
-            if self.pat[i] == s[m + i] {
+            if self.pat[i].as_ref() == Some(&s[m + i]) {
                 i += 1;
                 if i == self.pat.len() - 1 {
                     ret.push(m);
@@ -64,13 +64,24 @@ impl KMP {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use test_case::test_case;
 
-    #[test]
-    fn test() {
-        let kmp = KMP::new("aaa");
-        assert_eq!(kmp.matches("aaaaaaaa"), vec![0, 1, 2, 3, 4, 5]);
+    #[test_case("aaa", "aaaaaaaa")]
+    #[test_case("ab", "abracadabra")]
+    fn test(pat: &str, s: &str) {
+        let kmp = KMP::new(pat.as_bytes().to_owned());
+        let indices = kmp.matches(s.as_bytes());
 
-        let kmp = KMP::new("ab");
-        assert_eq!(kmp.matches("abracadabra"), vec![0, 7]);
+        assert_eq!(indices, bruteforce(pat, s));
+    }
+
+    fn bruteforce(pat: &str, s: &str) -> Vec<usize> {
+        let mut ret = vec![];
+        for i in 0..=s.len() - pat.len() {
+            if &s[i..i + pat.len()] == pat {
+                ret.push(i);
+            }
+        }
+        ret
     }
 }
