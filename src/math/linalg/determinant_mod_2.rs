c203661e8e904fbc2248268582676b1afd3ae74a
Haar-you <zemlya.pole@gmail.com>
2024-08-11 07:18:13 +0900

    [new] determinant mod 2

---
+++ src/math/linalg/determinant_mod_2.rs
@@ -0,0 +1,29 @@
+use crate::utils::bitset::*;
+
+pub fn determinant_mod_2(mut a: Vec<Bitset>) -> u64 {
+    let n = a.len();
+
+    assert!(a.iter().all(|r| r.len() == n));
+
+    for i in 0..n {
+        if !a[i].test(i) {
+            if let Some(j) = (i + 1..n).find(|&j| a[j].test(i)) {
+                a.swap(i, j);
+            } else {
+                return 0;
+            }
+        }
+        let ai = a.swap_remove(i);
+
+        for aj in a.iter_mut().skip(i) {
+            if aj.test(i) {
+                *aj ^= ai.clone();
+            }
+        }
+
+        a.push(ai);
+        a.swap(i, n - 1);
+    }
+
+    1
+}
--- src/math/linalg/mod.rs
+++ src/math/linalg/mod.rs
@@ -4,4 +4,5 @@ pub mod square_matrix;
 pub mod determinant_p;
 pub mod inverse_p;
 
+pub mod determinant_mod_2;
 pub mod inverse_mod_2;
