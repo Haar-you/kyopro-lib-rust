48604f411ac3e374de8b3c592f69b03905262a50
Haar-you <zemlya.pole@gmail.com>
2024-08-20 14:23:10 +0900

    [add] exponential

--- src/algebra/traits.rs
+++ src/algebra/traits.rs
@@ -32,3 +32,20 @@ impl<T: Monoid + Inverse> Group for T {}
 
 pub trait AbelianGroup: Group + Commutative {}
 impl<T: Group + Commutative> AbelianGroup for T {}
+
+pub trait Exponential<T: Clone>: BinaryOp<Output = T> + Identity {
+    fn exp(&self, mut a: Self::Output, mut n: u64) -> Self::Output {
+        let mut ret = self.id();
+
+        while n > 0 {
+            if n & 1 == 1 {
+                ret = self.op(ret, a.clone());
+            }
+            a = self.op(a.clone(), a);
+            n >>= 1;
+        }
+
+        ret
+    }
+}
+impl<T: Clone, A: BinaryOp<Output = T> + Identity> Exponential<T> for A {}
