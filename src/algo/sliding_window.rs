9adaa1993f31d39b32e000e49fb9bd88937d2f84
Haar-you <zemlya.pole@gmail.com>
2024-08-30 00:21:04 +0900

    [doc] sliding_window.rs

--- src/algo/sliding_window.rs
+++ src/algo/sliding_window.rs
@@ -4,8 +4,8 @@ use std::cmp::Reverse;
 use std::collections::VecDeque;
 
 /// 配列のすべての長さkの連続部分列について、その最小値を列挙する。
-/// # Complexity
-/// Time complexity $O(n)$
+///
+/// **Time complexity O(|a|)**
 pub fn sliding_minimum<T: Ord + Copy>(a: &[T], k: usize) -> Vec<T> {
     let n = a.len();
 
