ae62ebd9e378c46a904506fb1f51585a8f71530d
Haar-you <zemlya.pole@gmail.com>
2024-09-03 02:46:43 +0900

    [refactor] chinese_postman_problem

--- src/graph/chinese_postman.rs
+++ src/graph/chinese_postman.rs
@@ -4,12 +4,12 @@
 
 use crate::graph::*;
 use crate::num::one_zero::Zero;
-use std::{cmp::min, ops::Add};
+use std::ops::Add;
 
 /// **Time complexity O(V²2ⱽ)**
 pub fn chinese_postman_problem<T, E: EdgeTrait<Weight = T>>(g: &Graph<Undirected, E>) -> T
 where
-    T: Zero<Output = T> + Copy + Ord + Add<Output = T>,
+    T: Copy + Ord + Add<Output = T> + Zero<Output = T>,
 {
     let n = g.len();
     let zero = T::zero();
@@ -23,7 +23,7 @@ where
     for i in 0..n {
         for e in &g.edges[i] {
             let (from, to, cost) = (e.from(), e.to(), e.weight());
-            dist[from][to] = Some(dist[from][to].map_or(cost, |x| min(x, cost)));
+            dist[from][to] = dist[from][to].map(|x| x.min(cost)).or(Some(cost));
         }
     }
 
@@ -31,19 +31,19 @@ where
         for i in 0..n {
             for j in 0..n {
                 if let (Some(x), Some(y)) = (dist[i][k], dist[k][j]) {
-                    dist[i][j] = Some(dist[i][j].map_or(x + y, |z| min(z, x + y)));
+                    dist[i][j] = dist[i][j].map(|z| z.min(x + y)).or(Some(x + y))
                 }
             }
         }
     }
 
-    let mut odd = vec![];
-    for i in 0..n {
-        if g.edges[i].len() % 2 == 1 {
-            odd.push(i);
-        }
-    }
-
+    let odd: Vec<_> = g
+        .edges
+        .iter()
+        .enumerate()
+        .filter(|(_, es)| es.len() % 2 == 1)
+        .map(|(i, _)| i)
+        .collect();
     let m = odd.len();
 
     let mut dp = vec![None; 1 << m];
@@ -58,24 +58,16 @@ where
 
                 if let Some(d) = dp[i ^ (1 << j) ^ (1 << k)] {
                     let d = d + dist[odd[j]][odd[k]].unwrap();
-                    dp[i] = Some(dp[i].map_or(d, |x| min(x, d)));
+                    dp[i] = dp[i].map(|x| x.min(d)).or(Some(d));
                 }
             }
         }
     }
 
-    let mut ret = zero;
-
-    for i in 0..n {
-        for e in &g.edges[i] {
-            let (from, to, cost) = (e.from(), e.to(), e.weight());
-            if from <= to {
-                ret = ret + cost;
-            }
-        }
-    }
-
-    ret + dp[(1 << m) - 1].unwrap()
+    g.edges
+        .iter()
+        .flat_map(|es| es.iter().filter(|e| e.from() <= e.to()).map(|e| e.weight()))
+        .fold(dp[(1 << m) - 1].unwrap(), |x, y| x + y)
 }
 
 #[cfg(test)]
