6c813db26699296165eb25009fd9c6a82feb9104
Haar-you <zemlya.pole@gmail.com>
2024-09-10 19:54:20 +0900

    [update] join_str

--- src/iter/join_str.rs
+++ src/iter/join_str.rs
@@ -1,13 +1,11 @@
-pub trait JoinStr {
-    fn join_str(self, _: &str) -> String;
-}
-
-impl<T, I> JoinStr for I
-where
-    T: ToString,
-    I: Iterator<Item = T>,
-{
-    fn join_str(self, s: &str) -> String {
+pub trait JoinStr: Iterator {
+    fn join_str(self, s: &str) -> String
+    where
+        Self: Sized,
+        Self::Item: ToString,
+    {
         self.map(|x| x.to_string()).collect::<Vec<_>>().join(s)
     }
 }
+
+impl<I> JoinStr for I where I: Iterator + ?Sized {}
