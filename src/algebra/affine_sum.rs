90df6e67008f86077db288856029bcadd8c76f03
Haar-you <zemlya.pole@gmail.com>
2024-10-24 21:42:00 +0900

    [change] afiine_sum

--- src/algebra/affine_sum.rs
+++ src/algebra/affine_sum.rs
@@ -8,21 +8,22 @@ use std::{
 
 /// Range Affine Range Sum用の代数的構造
 #[derive(Copy, Clone, Default, Debug, PartialEq, Eq)]
-pub struct AffineSum<T>(PhantomData<T>);
+pub struct AffineSum<T, U = T>(PhantomData<(T, U)>);
 
-impl<T> AffineSum<T> {
-    /// `AffineSum<T>`を生成する。
+impl<T, U> AffineSum<T, U> {
+    /// `AffineSum<T, U>`を生成する。
     pub fn new() -> Self {
         Self(PhantomData)
     }
 }
 
-impl<T> Action for AffineSum<T>
+impl<T, U> Action for AffineSum<T, U>
 where
-    T: Add<Output = T> + Mul<Output = T> + Zero + One + Copy + From<usize>,
+    T: Add<Output = T> + Mul<Output = T> + Zero + Copy + From<U>,
+    U: Add<Output = U> + Mul<Output = U> + Zero + One + Copy + From<u64>,
 {
     type Output = T;
-    type Lazy = (T, T);
+    type Lazy = (U, U);
     fn fold_id(&self) -> Self::Output {
         T::zero()
     }
@@ -30,12 +31,12 @@ where
         left + right
     }
     fn update_id(&self) -> Self::Lazy {
-        (T::one(), T::zero())
+        (U::one(), U::zero())
     }
     fn update(&self, next: Self::Lazy, cur: Self::Lazy) -> Self::Lazy {
         (next.0 * cur.0, next.0 * cur.1 + next.1)
     }
     fn convert(&self, value: Self::Output, lazy: Self::Lazy, len: usize) -> Self::Output {
-        lazy.0 * value + lazy.1 * T::from(len)
+        T::from(lazy.0) * value + T::from(lazy.1 * U::from(len as u64))
     }
 }
