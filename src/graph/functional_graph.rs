17cec60bccd75d0ecb82b6c4126e44a8f2620a04
Haar-you <zemlya.pole@gmail.com>
2025-03-22 14:39:29 +0900

    [change] functional graph

--- src/graph/functional_graph.rs
+++ src/graph/functional_graph.rs
@@ -1,34 +1,78 @@
+//! Functional Graph
+//!
+//! # Problems
+//! - <https://atcoder.jp/contests/abc357/tasks/abc357_e>
+
 use crate::ds::unionfind::UnionFind;
 
+/// [`FunctionalGraph`]を構築するための構造体。
 pub struct FunctionalGraphBuilder {
     next: Vec<Option<usize>>,
 }
 
+/// 頂点の種類
 #[derive(Clone, Copy, PartialEq, Debug)]
 pub enum Kind {
-    Loop(usize),
-    Branch(usize),
-    Leaf(usize),
+    /// 閉路を構成している頂点。
+    Loop,
+    /// `Loop`でも`Leaf`でもない頂点。
+    Branch,
+    /// 入次数が`0`の頂点。
+    Leaf,
 }
 
+impl Kind {
+    /// `Loop`ならば`true`を返す。
+    pub fn is_loop(self) -> bool {
+        matches!(self, Self::Loop)
+    }
+
+    /// `Branch`ならば`true`を返す。
+    pub fn is_branch(self) -> bool {
+        matches!(self, Self::Branch)
+    }
+
+    /// `Leaf`ならば`true`を返す。
+    pub fn is_leaf(self) -> bool {
+        matches!(self, Self::Leaf)
+    }
+}
+
+/// 連結成分
+#[derive(Clone, Default, Debug)]
+pub struct Group {
+    /// 閉路
+    pub loops: Vec<usize>,
+    /// 枝
+    pub branches: Vec<usize>,
+    /// 葉
+    pub leaves: Vec<usize>,
+}
+
+/// 各頂点の出次数が`1`である、`n`頂点`n`辺の有向グラフ
 pub struct FunctionalGraph {
     next: Vec<usize>,
     v_kind: Vec<Kind>,
-    g_num: usize,
+    group_index: Vec<usize>,
+    groups: Vec<Group>,
+    children: Vec<Vec<usize>>,
 }
 
 impl FunctionalGraphBuilder {
+    /// `n`頂点の空なグラフを用意する。
     pub fn new(n: usize) -> Self {
         Self {
             next: vec![None; n],
         }
     }
 
+    /// `from`から`to`への有向辺を追加する。
     pub fn add(&mut self, from: usize, to: usize) {
         assert!(self.next[from].is_none());
         self.next[from] = Some(to);
     }
 
+    /// [`FunctionalGraph`]を構築する。
     pub fn build(self) -> FunctionalGraph {
         assert!(self.next.iter().all(|a| a.is_some()));
 
@@ -49,6 +93,9 @@ impl FunctionalGraphBuilder {
             .map(|(i, index)| *index = i)
             .count();
 
+        let mut groups = vec![Group::default(); g_num];
+        let mut group_index = vec![0; n];
+
         let mut in_deg = vec![0; n];
         for &next in &next {
             in_deg[next] += 1;
@@ -59,12 +106,18 @@ impl FunctionalGraphBuilder {
             .iter()
             .enumerate()
             .filter_map(|(i, &d)| (d == 0).then_some(i))
-            .inspect(|&i| v_kind[i] = Some(Kind::Leaf(index[uf.root_of(i)])))
+            .inspect(|&i| {
+                v_kind[i] = Some(Kind::Leaf);
+                group_index[i] = index[uf.root_of(i)];
+                groups[group_index[i]].leaves.push(i);
+            })
             .collect::<Vec<_>>();
 
         while let Some(cur) = stack.pop() {
             if v_kind[cur].is_none() {
-                v_kind[cur] = Some(Kind::Branch(index[uf.root_of(cur)]));
+                v_kind[cur] = Some(Kind::Branch);
+                group_index[cur] = index[uf.root_of(cur)];
+                groups[group_index[cur]].branches.push(cur);
             }
 
             let next = next[cur];
@@ -76,42 +129,57 @@ impl FunctionalGraphBuilder {
 
         for i in 0..n {
             if in_deg[i] != 0 {
-                v_kind[i] = Some(Kind::Loop(index[uf.root_of(i)]));
+                v_kind[i] = Some(Kind::Loop);
+                group_index[i] = index[uf.root_of(i)];
+                groups[group_index[i]].loops.push(i);
+            }
+        }
+
+        let mut children = vec![vec![]; n];
+        for (i, &p) in next.iter().enumerate() {
+            if !v_kind[i].unwrap().is_loop() {
+                children[p].push(i);
             }
         }
 
         FunctionalGraph {
             next,
             v_kind: v_kind.into_iter().flatten().collect(),
-            g_num,
+            group_index,
+            groups,
+            children,
         }
     }
 }
 
 impl FunctionalGraph {
+    /// 頂点`i`から辺を辿った次の頂点を返す。
     pub fn next_of(&self, i: usize) -> usize {
         self.next[i]
     }
 
+    /// 頂点`i`の種類を返す。
     pub fn kind_of(&self, i: usize) -> Kind {
         self.v_kind[i]
     }
 
-    pub fn grp_index_of(&self, i: usize) -> usize {
-        match self.v_kind[i] {
-            Kind::Loop(x) | Kind::Branch(x) | Kind::Leaf(x) => x,
-        }
+    /// 頂点`i`が属する連結成分に割り当てられた番号を返す。
+    pub fn group_index_of(&self, i: usize) -> usize {
+        self.group_index[i]
     }
 
-    pub fn loops(&self) -> Vec<Vec<usize>> {
-        let mut ret = vec![vec![]; self.g_num];
+    /// 頂点`i`の属する連結成分を返す。
+    pub fn group_of(&self, i: usize) -> &Group {
+        &self.groups[self.group_index_of(i)]
+    }
 
-        for (i, a) in self.v_kind.iter().enumerate() {
-            if let &Kind::Loop(x) = a {
-                ret[x].push(i);
-            }
-        }
+    /// すべての連結成分への参照を返す。
+    pub fn groups(&self) -> &[Group] {
+        &self.groups
+    }
 
-        ret
+    /// 閉路を切断して、根付き森として見たときの、頂点`i`の子頂点列を返す。
+    pub fn children<'a>(&'a self, i: usize) -> impl Iterator<Item = usize> + 'a {
+        self.children[i].iter().map(|&x| x)
     }
 }
