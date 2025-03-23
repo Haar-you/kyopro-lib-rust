38ae9967d42e14507e86d548549050243fd1e55f
Haar-you <zemlya.pole@gmail.com>
2025-03-23 22:49:33 +0900

    [change] mo

--- src/algo/mo.rs
+++ src/algo/mo.rs
@@ -1,12 +1,16 @@
 //! Mo's algorithm
+//!
+//! # Problems
+//! - <https://atcoder.jp/contests/dwacon2017-honsen/tasks/dwango2017final_b>
+
+/// Mo's algorithm
 pub struct Mo<'a> {
-    append_left: Box<dyn 'a + Fn(usize)>,
-    append_right: Box<dyn 'a + Fn(usize)>,
-    remove_left: Box<dyn 'a + Fn(usize)>,
-    remove_right: Box<dyn 'a + Fn(usize)>,
-    query: Box<dyn 'a + Fn(usize)>,
+    append_left: Box<dyn 'a + FnMut(usize)>,
+    append_right: Box<dyn 'a + FnMut(usize)>,
+    remove_left: Box<dyn 'a + FnMut(usize)>,
+    remove_right: Box<dyn 'a + FnMut(usize)>,
+    query: Box<dyn 'a + FnMut(usize)>,
     q: usize,
-    index: usize,
     width: usize,
     left: Vec<usize>,
     right: Vec<usize>,
@@ -14,14 +18,14 @@ pub struct Mo<'a> {
 }
 
 impl<'a> Mo<'a> {
+    /// クエリ処理用の関数を登録して[`Mo`]を作る。
     pub fn new(
         n: usize,
-        q: usize,
-        append_left: Box<impl 'a + Fn(usize)>,
-        append_right: Box<impl 'a + Fn(usize)>,
-        remove_left: Box<impl 'a + Fn(usize)>,
-        remove_right: Box<impl 'a + Fn(usize)>,
-        query: Box<impl 'a + Fn(usize)>,
+        append_left: Box<impl 'a + FnMut(usize)>,
+        append_right: Box<impl 'a + FnMut(usize)>,
+        remove_left: Box<impl 'a + FnMut(usize)>,
+        remove_right: Box<impl 'a + FnMut(usize)>,
+        query: Box<impl 'a + FnMut(usize)>,
     ) -> Self {
         Self {
             append_left,
@@ -29,22 +33,23 @@ impl<'a> Mo<'a> {
             remove_left,
             remove_right,
             query,
-            q,
-            index: 0,
+            q: 0,
             width: (n as f64).sqrt() as usize,
-            left: vec![0; q],
-            right: vec![0; q],
-            ord: vec![0; q],
+            left: vec![],
+            right: vec![],
+            ord: vec![],
         }
     }
 
+    /// 範囲`[l, r)`を登録する。
     pub fn add(&mut self, l: usize, r: usize) {
-        self.left[self.index] = l;
-        self.right[self.index] = r;
-        self.ord[self.index] = self.index;
-        self.index += 1;
+        self.left.push(l);
+        self.right.push(r);
+        self.ord.push(self.q);
+        self.q += 1;
     }
 
+    /// 登録した範囲をすべて訪れるように実行する。
     pub fn run(mut self) {
         let left = self.left;
         let right = self.right;
@@ -68,23 +73,22 @@ impl<'a> Mo<'a> {
         let mut l = left[self.ord[0]];
         let mut r = left[self.ord[0]];
 
-        for q in 0..self.q {
-            let id = self.ord[q];
+        for id in self.ord {
+            let left = left[id];
+            let right = right[id];
 
-            while l != left[id] || r != right[id] {
-                if l > left[id] {
+            while l != left || r != right {
+                if l > left {
                     l -= 1;
                     (self.append_left)(l);
-                }
-                if l < left[id] {
+                } else if l < left {
                     (self.remove_left)(l);
                     l += 1;
                 }
-                if r < right[id] {
+                if r < right {
                     (self.append_right)(r);
                     r += 1;
-                }
-                if r > right[id] {
+                } else if r > right {
                     r -= 1;
                     (self.remove_right)(r);
                 }
