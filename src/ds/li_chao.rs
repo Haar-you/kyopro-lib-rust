226a1be95e5620ca5a566c756de7613c6c1c3038
Haar-you <zemlya.pole@gmail.com>
2025-03-29 11:20:55 +0900

    [change] lichaotree xsが空のときに対応

--- src/ds/li_chao.rs
+++ src/ds/li_chao.rs
@@ -70,24 +70,28 @@ impl<T: Elem> LiChaoTree<T> {
     }
 
     /// `query`で使用する点列`xs`と最大値クエリ/最小値クエリを指定して[`LiChaoTree`]を構築する。
-    pub fn new(mut xs: Vec<T>, mode: Mode) -> Self {
-        xs.sort();
-        xs.dedup();
+    pub fn new(mut xs: Vec<T>, mode: Mode) -> Result<Self, &'static str> {
+        if xs.is_empty() {
+            Err("`xs`が空なので、`LiChaoTree`は構築されない。")
+        } else {
+            xs.sort();
+            xs.dedup();
 
-        let size = xs.len().next_power_of_two();
+            let size = xs.len().next_power_of_two();
 
-        xs.resize(size, *xs.last().unwrap());
+            xs.resize(size, *xs.last().unwrap());
 
-        let data = vec![None; size * 2];
-        let mut range = vec![(0, 0); size * 2];
-        Self::init_range(&mut range, size, 1, 0, size);
+            let data = vec![None; size * 2];
+            let mut range = vec![(0, 0); size * 2];
+            Self::init_range(&mut range, size, 1, 0, size);
 
-        Self {
-            xs,
-            size,
-            data,
-            range,
-            mode,
+            Ok(Self {
+                xs,
+                size,
+                data,
+                range,
+                mode,
+            })
         }
     }
 
