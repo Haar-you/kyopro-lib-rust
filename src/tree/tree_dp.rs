bbe3cab78c6e16f2b173cd29d05d0bfe5d7856d1
Haar-you <zemlya.pole@gmail.com>
2025-04-10 14:51:39 +0900

    [change] tree_dp, rerooting

--- src/tree/tree_dp.rs
+++ src/tree/tree_dp.rs
@@ -7,23 +7,23 @@ use crate::tree::*;
 /// # Problems
 /// - <https://atcoder.jp/contests/dp/tasks/dp_p>
 /// - <https://yukicoder.me/problems/no/763>
-pub struct TreeDP<'a, Weight, T, U> {
+pub struct TreeDP<'a, T, U, E> {
     init: U,
-    up: Box<dyn 'a + Fn(T, (usize, Weight)) -> U>,
+    up: Box<dyn 'a + Fn(T, &'a E) -> U>,
     merge: Box<dyn 'a + Fn(U, U) -> U>,
     apply: Box<dyn 'a + Fn(U, usize) -> T>,
 }
 
-impl<'a, Weight, T, U> TreeDP<'a, Weight, T, U>
+impl<'a, T, U, E> TreeDP<'a, T, U, E>
 where
-    Weight: Copy,
+    E: TreeEdgeTrait,
     T: Clone,
     U: Clone,
 {
     /// `TreeDP`を構築する。
     pub fn new(
         init: U,
-        up: Box<impl 'a + Fn(T, (usize, Weight)) -> U>,
+        up: Box<impl 'a + Fn(T, &'a E) -> U>,
         merge: Box<impl 'a + Fn(U, U) -> U>,
         apply: Box<impl 'a + Fn(U, usize) -> T>,
     ) -> Self {
@@ -38,7 +38,7 @@ where
     /// `root`を根にして、`tree`上でDPを実行する。
     ///
     /// **Time complexity** $O(n)$
-    pub fn run<E: TreeEdgeTrait<Weight = Weight>>(&self, tree: &Tree<E>, root: usize) -> Vec<T> {
+    pub fn run(&self, tree: &'a Tree<E>, root: usize) -> Vec<T> {
         let size = tree.len();
         let mut ret = vec![None; size];
 
@@ -47,9 +47,9 @@ where
         ret.into_iter().flatten().collect()
     }
 
-    fn __dfs<E: TreeEdgeTrait<Weight = Weight>>(
+    fn __dfs(
         &self,
-        tree: &Tree<E>,
+        tree: &'a Tree<E>,
         cur: usize,
         par: Option<usize>,
         ret: &mut Vec<Option<T>>,
@@ -59,7 +59,7 @@ where
             .filter(|e| par.is_none_or(|p| p != e.to()))
             .map(|e| {
                 let a = self.__dfs(tree, e.to(), Some(cur), ret);
-                (self.up)(a, (e.to(), e.weight()))
+                (self.up)(a, e)
             })
             .fold(self.init.clone(), |a, b| (self.merge)(a, b));
         ret[cur] = Some((self.apply)(acc, cur));
