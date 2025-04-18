c72e8dd4d3759c36c16f34ab9750bb54158d8c6b
Haar-you <zemlya.pole@gmail.com>
2025-04-18 20:27:14 +0900

    [update] impl_ops

--- src/macros/impl_ops.rs
+++ src/macros/impl_ops.rs
@@ -3,65 +3,70 @@
 /// [`Add`](std::ops::Add), [`Sub`](std::ops::Sub), [`Mul`](std::ops::Mul), [`Div`](std::ops::Div), [`Rem`](std::ops::Rem), [`AddAssign`](std::ops::AddAssign), [`SubAssign`](std::ops::SubAssign), [`MulAssign`](std::ops::MulAssign), [`DivAssign`](std::ops::DivAssign), [`RemAssign`](std::ops::RemAssign), [`Neg`](std::ops::Neg)を実装する。
 #[macro_export]
 macro_rules! impl_ops {
-    (@inner, $(#[$meta:meta])* [$($bound:tt)*]; $tr:ty, $a:ty, $f:expr, $fn:tt) => {
+    (@inner, $(#[$meta:meta])* [$($bound:tt)*]; $tr:ty, $rhs:ty, $a:ty, $f:expr, $fn:tt) => {
         impl <$($bound)*> $tr for $a {
             type Output = Self;
             $(#[$meta])*
-            fn $fn(self, rhs: Self) -> Self::Output {
+            fn $fn(self, rhs: $rhs) -> Self::Output {
                 $f(self, rhs)
             }
         }
     };
-    (@inner_assign, $(#[$meta:meta])* [$($bound:tt)*]; $tr:ty, $a:ty, $f:expr, $fn:tt) => {
+    (@inner_assign, $(#[$meta:meta])* [$($bound:tt)*]; $tr:ty, $rhs:ty, $a:ty, $f:expr, $fn:tt) => {
         impl <$($bound)*> $tr for $a {
             $(#[$meta])*
-            fn $fn(&mut self, rhs: Self) {
+            fn $fn(&mut self, rhs: $rhs) {
                 $f(self, rhs)
             }
         }
     };
 
     ($(#[$meta:meta])* [$($bound:tt)*]; $trait:ident for $a:ty, $f:expr) => {
-        impl_ops!(@when $(#[$meta])* [$($bound)*]; $trait, $a, $f);
+        impl_ops!(@when $(#[$meta])* [$($bound)*]; $trait, Self, $a, $f);
+    };
+    ($(#[$meta:meta])* [$($bound:tt)*]; $trait:ident <$rhs:ty> for $a:ty, $f:expr) => {
+        impl_ops!(@when $(#[$meta])* [$($bound)*]; $trait, $rhs, $a, $f);
     };
     ($(#[$meta:meta])* $trait:ident for $a:ty, $f:expr) => {
-        impl_ops!(@when $(#[$meta])* []; $trait, $a, $f);
+        impl_ops!(@when $(#[$meta])* []; $trait, Self, $a, $f);
+    };
+    ($(#[$meta:meta])* $trait:ident <$rhs:ty> for $a:ty, $f:expr) => {
+        impl_ops!(@when $(#[$meta])* []; $trait, $rhs, $a, $f);
     };
 
-
-    (@when $(#[$meta:meta])* [$($bound:tt)*]; Add, $a:ty, $f:expr) => {
-        impl_ops!(@inner, $(#[$meta])* [$($bound)*]; std::ops::Add, $a, $f, add);
+    (@when $(#[$meta:meta])* [$($bound:tt)*]; Add, $rhs:ty, $a:ty, $f:expr) => {
+        impl_ops!(@inner, $(#[$meta])* [$($bound)*]; std::ops::Add<$rhs>, $rhs, $a, $f, add);
     };
-    (@when $(#[$meta:meta])* [$($bound:tt)*]; Sub, $a:ty, $f:expr) => {
-        impl_ops!(@inner, $(#[$meta])* [$($bound)*]; std::ops::Sub, $a, $f, sub);
+    (@when $(#[$meta:meta])* [$($bound:tt)*]; Sub, $rhs:ty, $a:ty, $f:expr) => {
+        impl_ops!(@inner, $(#[$meta])* [$($bound)*]; std::ops::Sub<$rhs>, $rhs, $a, $f, sub);
     };
-    (@when $(#[$meta:meta])* [$($bound:tt)*]; Mul, $a:ty, $f:expr) => {
-        impl_ops!(@inner, $(#[$meta])* [$($bound)*]; std::ops::Mul, $a, $f, mul);
+    (@when $(#[$meta:meta])* [$($bound:tt)*]; Mul, $rhs:ty, $a:ty, $f:expr) => {
+        impl_ops!(@inner, $(#[$meta])* [$($bound)*]; std::ops::Mul<$rhs>, $rhs, $a, $f, mul);
     };
-    (@when $(#[$meta:meta])* [$($bound:tt)*]; Div, $a:ty, $f:expr) => {
-        impl_ops!(@inner, $(#[$meta])* [$($bound)*]; std::ops::Div, $a, $f, div);
+    (@when $(#[$meta:meta])* [$($bound:tt)*]; Div, $rhs:ty, $a:ty, $f:expr) => {
+        impl_ops!(@inner, $(#[$meta])* [$($bound)*]; std::ops::Div<$rhs>, $rhs, $a, $f, div);
     };
-    (@when $(#[$meta:meta])* [$($bound:tt)*]; Rem, $a:ty, $f:expr) => {
-        impl_ops!(@inner, $(#[$meta])* [$($bound)*]; std::ops::Rem, $a, $f, rem);
-    };
-
-    (@when $(#[$meta:meta])* [$($bound:tt)*]; AddAssign, $a:ty, $f:expr) => {
-        impl_ops!(@inner_assign, $(#[$meta])* [$($bound)*]; std::ops::AddAssign, $a, $f, add_assign);
-    };
-    (@when $(#[$meta:meta])* [$($bound:tt)*]; SubAssign, $a:ty, $f:expr) => {
-        impl_ops!(@inner_assign, $(#[$meta])* [$($bound)*]; std::ops::SubAssign, $a, $f, sub_assign);
-    };
-    (@when $(#[$meta:meta])* [$($bound:tt)*]; MulAssign, $a:ty, $f:expr) => {
-        impl_ops!(@inner_assign, $(#[$meta])* [$($bound)*]; std::ops::MulAssign, $a, $f, mul_assign);
-    };
-    (@when $(#[$meta:meta])* [$($bound:tt)*]; DivAssign, $a:ty, $f:expr) => {
-        impl_ops!(@inner_assign, $(#[$meta])* [$($bound)*]; std::ops::DivAssign, $a, $f, div_assign);
-    };
-    (@when $(#[$meta:meta])* [$($bound:tt)*]; RemAssign, $a:ty, $f:expr) => {
-        impl_ops!(@inner_assign, $(#[$meta])* [$($bound)*]; std::ops::RemAssign, $a, $f, rem_assign);
+    (@when $(#[$meta:meta])* [$($bound:tt)*]; Rem, $rhs:ty, $a:ty, $f:expr) => {
+        impl_ops!(@inner, $(#[$meta])* [$($bound)*]; std::ops::Rem<$rhs>, $rhs, $a, $f, rem);
     };
 
-    (@when $(#[$meta:meta])* [$($bound:tt)*]; Neg, $a:ty, $f:expr) => {
+    (@when $(#[$meta:meta])* [$($bound:tt)*]; AddAssign, $rhs:ty, $a:ty, $f:expr) => {
+        impl_ops!(@inner_assign, $(#[$meta])* [$($bound)*]; std::ops::AddAssign<$rhs>, $rhs, $a, $f, add_assign);
+    };
+    (@when $(#[$meta:meta])* [$($bound:tt)*]; SubAssign, $rhs:ty, $a:ty, $f:expr) => {
+        impl_ops!(@inner_assign, $(#[$meta])* [$($bound)*]; std::ops::SubAssign<$rhs>, $rhs, $a, $f, sub_assign);
+    };
+    (@when $(#[$meta:meta])* [$($bound:tt)*]; MulAssign, $rhs:ty, $a:ty, $f:expr) => {
+        impl_ops!(@inner_assign, $(#[$meta])* [$($bound)*]; std::ops::MulAssign<$rhs>, $rhs, $a, $f, mul_assign);
+    };
+    (@when $(#[$meta:meta])* [$($bound:tt)*]; DivAssign, $rhs:ty, $a:ty, $f:expr) => {
+        impl_ops!(@inner_assign, $(#[$meta])* [$($bound)*]; std::ops::DivAssign<$rhs>, $rhs, $a, $f, div_assign);
+    };
+    (@when $(#[$meta:meta])* [$($bound:tt)*]; RemAssign, $rhs:ty, $a:ty, $f:expr) => {
+        impl_ops!(@inner_assign, $(#[$meta])* [$($bound)*]; std::ops::RemAssign<$rhs>, $rhs, $a, $f, rem_assign);
+    };
+
+    (@when $(#[$meta:meta])* [$($bound:tt)*]; Neg, $rhs:ty, $a:ty, $f:expr) => {
         impl <$($bound)*> std::ops::Neg for $a {
             type Output = Self;
             $(#[$meta])*
