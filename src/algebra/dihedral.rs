30efc5fbba4aeee456f6356c5e5302fda44965b6
Haar-you <zemlya.pole@gmail.com>
2025-05-26 23:46:09 +0900

    [change] dihedral group

--- src/algebra/dihedral.rs
+++ src/algebra/dihedral.rs
@@ -1,70 +1,71 @@
-//! 二面体群
+//! 二面体群 $D_n$
 //!
 //! # Problems
 //! - <https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0390>
 
 pub use crate::algebra::traits::*;
 
-/// 二面体群の元
+/// 二面体群$D_n$の元
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum DihedralValue {
     /// 回転
-    R(usize, usize),
+    R(usize),
     /// 鏡映
-    S(usize, usize),
+    S(usize),
 }
 
-impl DihedralValue {
-    fn get_k(self) -> usize {
-        match self {
-            Self::R(_, k) => k,
-            Self::S(_, k) => k,
-        }
-    }
+use DihedralValue::{R, S};
 
-    fn op(a: Self, b: Self) -> Self {
-        let k = a.get_k();
-        assert_eq!(k, b.get_k());
-
-        match (a, b) {
-            (Self::R(x, _), Self::R(y, _)) => Self::R((x + y) % k, k),
-            (Self::R(x, _), Self::S(y, _)) => Self::S((k + y - x) % k, k),
-            (Self::S(x, _), Self::R(y, _)) => Self::S((x + y) % k, k),
-            (Self::S(x, _), Self::S(y, _)) => Self::R((k + y - x) % k, k),
-        }
-    }
-}
-
-/// 二面体群
-#[derive(Clone, Copy, Debug, Default)]
-pub enum Dihedral {
-    #[default]
-    Id,
-    Value(DihedralValue),
-}
-
-impl Dihedral {
-    pub fn eval(self, k: usize) -> DihedralValue {
-        match self {
-            Self::Id => DihedralValue::R(0, k),
-            Self::Value(a) => a,
-        }
-    }
-
-    pub fn is_identity(self) -> bool {
-        match self {
-            Self::Id => true,
-            Self::Value(a) => matches!(a, DihedralValue::R(0, _)),
-        }
-    }
+/// 二面体群$D_n$
+#[derive(Clone, Copy, Debug)]
+pub struct Dihedral {
+    n: usize,
+    value: DihedralValue,
 }
 
 impl PartialEq for Dihedral {
     fn eq(&self, other: &Self) -> bool {
         match (self, other) {
-            (Self::Value(a), Self::Value(b)) => a == b,
-            _ => self.is_identity() && other.is_identity(),
+            (Self { n: 0, value: R(0) }, Self { value: R(0), .. }) => true,
+            (Self { value: R(0), .. }, Self { n: 0, value: R(0) }) => true,
+            _ => self.n == other.n && self.value == other.value,
         }
+    }
+}
+
+impl Dihedral {
+    fn _op(a: Self, b: Self) -> Self {
+        match (a.n, b.n) {
+            (0, _) => return b,
+            (_, 0) => return a,
+            _ => {}
+        }
+
+        let n = a.n;
+        assert_eq!(b.n, a.n);
+
+        let value = match (a.value, b.value) {
+            (R(x), R(y)) => R((x + y) % n),
+            (R(x), S(y)) => S((n + y - x) % n),
+            (S(x), R(y)) => S((x + y) % n),
+            (S(x), S(y)) => R((n + y - x) % n),
+        };
+
+        Self { n, value }
+    }
+
+    /// $D_n$の回転を表す元$R_i$を返す。
+    pub fn r(i: usize, n: usize) -> Self {
+        assert!(n > 0);
+        assert!(i < n);
+        Self { n, value: R(i) }
+    }
+
+    /// $D_n$の鏡映を表す元$S_i$を返す。
+    pub fn s(i: usize, n: usize) -> Self {
+        assert!(n > 0);
+        assert!(i < n);
+        Self { n, value: S(i) }
     }
 }
 
@@ -72,30 +73,28 @@ impl Set for Dihedral {}
 
 impl BinaryOp for Dihedral {
     fn op(self, b: Self) -> Self {
-        match (self, b) {
-            (Self::Value(a), Self::Value(b)) => Self::Value(DihedralValue::op(a, b)),
-            (a @ Self::Value(_), _) => a,
-            (_, b @ Self::Value(_)) => b,
-            _ => Self::Id,
-        }
+        Dihedral::_op(self, b)
     }
 }
 
 impl Identity for Dihedral {
     fn id() -> Self {
-        Self::Id
+        Self { n: 0, value: R(0) }
     }
 }
 
 impl Inverse for Dihedral {
     fn inv(self) -> Self {
-        match self {
-            Self::Id => self,
-            Self::Value(a) => Self::Value(match a {
-                DihedralValue::R(x, k) => DihedralValue::R(if x == 0 { 0 } else { k - 1 }, k),
-                DihedralValue::S(_, _) => a,
-            }),
-        }
+        let Self { n, value } = self;
+        let value = if n != 0 {
+            match value {
+                R(x) => R(if x == 0 { 0 } else { n - x }),
+                S(_) => value,
+            }
+        } else {
+            value
+        };
+        Self { n, value }
     }
 }
 
@@ -110,19 +109,23 @@ mod tests {
         let k = 3;
 
         let a = (0..k)
-            .map(|i| DihedralValue::R(i, k))
-            .chain((0..k).map(|i| DihedralValue::S(i, k)))
+            .map(|i| Dihedral::r(i, k))
+            .chain((0..k).map(|i| Dihedral::s(i, k)))
             .collect::<Vec<_>>();
 
-        for l in &a {
-            for r in &a {
-                let l = Dihedral::Value(*l);
-                let r = Dihedral::Value(*r);
-
+        for &l in &a {
+            for &r in &a {
                 let x = r.op(l);
 
                 println!("{:?} {:?} = {:?}", l, r, x)
             }
+        }
+
+        for x in a {
+            let y = x.inv();
+
+            println!("{:?} {:?} {:?}", x, y, x.op(y));
+            assert_eq!(x.op(y), Dihedral::id());
         }
     }
 }
