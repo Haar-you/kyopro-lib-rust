bdca20b5ff942f7338d6b188dbc5af6a5fbd60a3
Haar-you <zemlya.pole@gmail.com>
2025-05-28 18:22:11 +0900

    [update] one_zero

--- src/num/one_zero.rs
+++ src/num/one_zero.rs
@@ -12,16 +12,14 @@ pub trait One {
     fn one() -> Self;
 }
 
+use std::num::Wrapping;
 macro_rules! impl_one_zero {
-    ($($t:ty),*) => {
+    ($($t:tt),*) => {
         $(
-            impl Zero for $t {
-                fn zero() -> Self { 0 as $t }
-            }
-
-            impl One for $t {
-                fn one() -> Self { 1 as $t }
-            }
+            impl Zero for $t { fn zero() -> Self { 0 as $t } }
+            impl One for $t { fn one() -> Self { 1 as $t } }
+            impl Zero for Wrapping<$t> { fn zero() -> Self { Wrapping($t::zero()) }}
+            impl One for Wrapping<$t> { fn one() -> Self { Wrapping($t::one()) }}
         )*
     }
 }
