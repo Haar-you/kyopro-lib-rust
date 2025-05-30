4bfb0d5f44f36f5a27ec094e26ac270e8c37e392
Haar-you <zemlya.pole@gmail.com>
2025-05-30 23:34:39 +0900

    [change] dual segtree 不変であるべきメソッドをmutなしで呼べるようにした。

--- src/ds/dual_segtree.rs
+++ src/ds/dual_segtree.rs
@@ -3,35 +3,53 @@
 
 pub use crate::algebra::traits::Monoid;
 use crate::misc::range::range_bounds_to_range;
+use std::cell::RefCell;
 use std::ops::RangeBounds;
 
 /// モノイド列の区間更新・点取得($O(\log n)$, $O(\log n)$)ができる。
 pub struct DualSegtree<M: Monoid> {
     original_size: usize,
     size: usize,
-    data: Vec<M>,
+    data: RefCell<Vec<M>>,
 }
 
 impl<M: Monoid + Clone> DualSegtree<M> {
     /// **Time complexity** $O(n)$
     pub fn new(n: usize) -> Self {
         let size = n.next_power_of_two() * 2;
+        let data = RefCell::new(vec![M::id(); size]);
         DualSegtree {
             original_size: n,
             size,
-            data: vec![M::id(); size],
+            data,
         }
     }
 
-    fn propagate(&mut self, i: usize) {
+    /// スライスで初期化する。
+    pub fn from_slice(a: &[M]) -> Self {
+        let size = a.len().next_power_of_two() * 2;
+        let mut data = vec![M::id(); size];
+        for (i, e) in a.iter().enumerate() {
+            data[i + size / 2] = e.clone();
+        }
+        DualSegtree {
+            original_size: a.len(),
+            size,
+            data: RefCell::new(data),
+        }
+    }
+
+    fn propagate(&self, i: usize) {
+        let mut data = self.data.borrow_mut();
+
         if i < self.size / 2 {
-            self.data[i << 1] = M::op(self.data[i << 1].clone(), self.data[i].clone());
-            self.data[(i << 1) | 1] = M::op(self.data[(i << 1) | 1].clone(), self.data[i].clone());
-            self.data[i] = M::id();
+            data[i << 1] = M::op(data[i << 1].clone(), data[i].clone());
+            data[(i << 1) | 1] = M::op(data[(i << 1) | 1].clone(), data[i].clone());
+            data[i] = M::id();
         }
     }
 
-    fn propagate_top_down(&mut self, mut i: usize) {
+    fn propagate_top_down(&self, mut i: usize) {
         let mut temp = vec![];
         while i > 1 {
             i >>= 1;
@@ -44,26 +62,18 @@ impl<M: Monoid + Clone> DualSegtree<M> {
     }
 
     /// **Time complexity** $O(\log n)$
-    pub fn get(&mut self, i: usize) -> M {
+    pub fn get(&self, i: usize) -> M {
         self.propagate_top_down(i + self.size / 2);
-        self.data[i + self.size / 2].clone()
-    }
-
-    /// スライスで初期化する。
-    pub fn from_slice(&mut self, a: &[M]) {
-        self.data = vec![M::id(); self.size];
-        for (i, e) in a.iter().enumerate() {
-            self.data[i + self.size / 2] = e.clone();
-        }
+        self.data.borrow()[i + self.size / 2].clone()
     }
 
     /// 遅延操作を完了させたモノイド列を`Vec`で返す。
-    pub fn to_vec(&mut self) -> Vec<M> {
+    pub fn to_vec(&self) -> Vec<M> {
         for i in 1..self.size {
             self.propagate(i);
         }
 
-        self.data[self.size / 2..self.size / 2 + self.original_size].to_vec()
+        self.data.borrow()[self.size / 2..self.size / 2 + self.original_size].to_vec()
     }
 
     /// **Time complexity** $O(\log n)$
@@ -76,13 +86,15 @@ impl<M: Monoid + Clone> DualSegtree<M> {
         self.propagate_top_down(l);
         self.propagate_top_down(r);
 
+        let mut data = self.data.borrow_mut();
+
         while l < r {
             if (r & 1) == 1 {
                 r -= 1;
-                self.data[r] = M::op(self.data[r].clone(), value.clone());
+                data[r] = M::op(data[r].clone(), value.clone());
             }
             if (l & 1) == 1 {
-                self.data[l] = M::op(self.data[l].clone(), value.clone());
+                data[l] = M::op(data[l].clone(), value.clone());
                 l += 1;
             }
             l >>= 1;
@@ -100,12 +112,16 @@ mod tests {
 
     #[test]
     fn test() {
+        let mut rng = rand::thread_rng();
         let n = 100;
 
-        let mut a = vec![Sum::id(); n];
-        let mut seg = DualSegtree::<Sum<u32>>::new(n);
-
-        let mut rng = rand::thread_rng();
+        let mut a = (0..n)
+            .map(|_| {
+                let x = rng.gen_range(0..10000);
+                Sum(x)
+            })
+            .collect::<Vec<_>>();
+        let mut seg = DualSegtree::<Sum<u32>>::from_slice(&a);
 
         for _ in 0..100 {
             let lr = rand_range(&mut rng, 0..n);
