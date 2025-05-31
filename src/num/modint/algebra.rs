5bbdd23f3b7064517d518d10e835105ad263fc07
Haar-you <zemlya.pole@gmail.com>
2025-05-31 23:17:44 +0900

    [change] modint algebra

--- src/num/modint/algebra.rs
+++ src/num/modint/algebra.rs
@@ -5,44 +5,57 @@ use crate::{impl_algebra, num::modint::*};
 
 /// `mod m`上の加法
 #[derive(Clone, Copy, Default, Debug)]
-pub enum SumModM {
-    #[default]
-    Zero,
-    Value(ModInt),
+pub struct SumModM(Option<ModInt>);
+impl SumModM {
+    pub fn new(a: ModInt) -> Self {
+        Self(Some(a))
+    }
+    pub fn value(self) -> u32 {
+        self.0.map_or(0, |a| a.value)
+    }
 }
+impl PartialEq for SumModM {
+    fn eq(&self, other: &Self) -> bool {
+        self.value() == other.value()
+    }
+}
+impl Eq for SumModM {}
 impl_algebra!(
     SumModM;
-    op: |a: Self, b: Self| match (a, b) {
-        (Self::Value(a), Self::Value(b)) => Self::Value(a + b),
-        (Self::Value(_), _) => a,
-        (_, Self::Value(_)) => b,
-        _ => Self::Zero
+    op: |a: Self, b: Self| match (a.0, b.0) {
+        (Some(a), Some(b)) => Self(Some(a + b)),
+        (a, None) | (None, a) => Self(a),
     };
-    id: Self::Zero;
-    inv: |a: Self| match a {
-        Self::Value(a) => Self::Value(-a),
-        _ => Self::Zero
-    };
+    id: Self(None);
+    inv: |a: Self| Self(a.0.map(|x| -x));
     assoc;
     commu;
 );
 
 /// `mod m`上の乗法
 #[derive(Clone, Copy, Default, Debug)]
-pub enum ProdModM {
-    #[default]
-    One,
-    Value(ModInt),
+pub struct ProdModM(Option<ModInt>);
+impl ProdModM {
+    pub fn new(a: ModInt) -> Self {
+        Self(Some(a))
+    }
+    pub fn value(self) -> u32 {
+        self.0.map_or(1, |a| a.value)
+    }
 }
+impl PartialEq for ProdModM {
+    fn eq(&self, other: &Self) -> bool {
+        self.value() == other.value()
+    }
+}
+impl Eq for ProdModM {}
 impl_algebra!(
     ProdModM;
-    op: |a: Self, b: Self| match (a, b) {
-        (Self::Value(a), Self::Value(b)) => Self::Value(a * b),
-        (Self::Value(_), _) => a,
-        (_, Self::Value(_)) => b,
-        _ => Self::One
+    op: |a: Self, b: Self| match (a.0, b.0) {
+        (Some(a), Some(b)) => Self(Some(a * b)),
+        (a, None) | (None, a) => Self(a),
     };
-    id: Self::One;
+    id: Self(None);
     assoc;
     commu;
 );
