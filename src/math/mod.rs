4f1d95d3352c828f7e50fddbdd0ca56bdceebea3
Haar-you <zemlya.pole@gmail.com>
2025-08-22 18:23:11 +0900

    [new] stirling first table

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -19,6 +19,7 @@ pub mod binomial_coefficient;
 pub mod factorial;
 pub mod partition_number;
 pub mod stirling_first;
+pub mod stirling_first_table;
 pub mod stirling_second;
 pub mod stirling_second_table;
 
