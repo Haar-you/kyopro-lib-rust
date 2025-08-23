a67ee9e2b7eb541f89c2b3ebc9426784ef923793
Haar-you <zemlya.pole@gmail.com>
2025-08-23 19:53:17 +0900

    [new] bell_number

---
+++ src/math/bell_number.rs
@@ -0,0 +1,29 @@
+//! ベル数$B_0, \dots, B_n$を列挙する。
+use crate::{
+    math::{factorial::FactorialTable, fps::exp::FpsExp, polynomial::Polynomial},
+    num::const_modint::*,
+};
+
+/// ベル数$B_0, \dots, B_n$を列挙する。
+pub fn bell_number<Fps, const P: u32>(
+    n: usize,
+    ft: &FactorialTable<ConstModIntBuilder<P>>,
+    fps: &Fps,
+) -> Vec<ConstModInt<P>>
+where
+    Fps: FpsExp<Poly = Polynomial<P>>,
+{
+    let mut f = vec![ConstModInt::new(0); n + 1];
+
+    for i in 1..=n {
+        f[i] = ft.inv_facto(i);
+    }
+
+    let mut ret: Vec<_> = fps.fps_exp(f.into()).into();
+
+    for i in 0..=n {
+        ret[i] *= ft.facto(i);
+    }
+
+    ret
+}
