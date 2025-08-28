d4f6ef32711cec96f38611613da692fb08c63ece
Haar-you <zemlya.pole@gmail.com>
2025-08-28 19:28:25 +0900

    [update] sparse polynomial

--- src/math/sparse_polynomial.rs
+++ src/math/sparse_polynomial.rs
@@ -1,20 +1,16 @@
 //! 疎な多項式
-use std::collections::HashMap;
-
 use crate::num::const_modint::*;
 
 /// 疎な多項式
 #[derive(Clone, Debug, Default)]
 pub struct SparsePolynomial<const P: u32> {
-    pub(crate) data: HashMap<usize, ConstModInt<P>>,
+    pub(crate) data: Vec<(usize, ConstModInt<P>)>,
 }
 
 impl<const P: u32> SparsePolynomial<P> {
     /// 零多項式を得る。
     pub fn zero() -> Self {
-        Self {
-            data: HashMap::new(),
-        }
+        Self { data: vec![] }
     }
 
     /// 定数項のみをもつ多項式を生成する。
@@ -22,32 +18,56 @@ impl<const P: u32> SparsePolynomial<P> {
         if a.value() == 0 {
             Self::zero()
         } else {
-            let mut data = HashMap::new();
-            data.insert(0, a);
-            Self { data }
+            Self { data: vec![(0, a)] }
         }
     }
 
     pub fn add(&mut self, i: usize, x: ConstModInt<P>) {
-        *self.data.entry(i).or_default() += x;
+        for (j, y) in self.data.iter_mut() {
+            if i == *j {
+                *y += x;
+                return;
+            }
+        }
+        self.data.push((i, x));
     }
 
-    pub fn from_vec(a: Vec<(usize, ConstModInt<P>)>) -> Self {
-        let mut data = HashMap::new();
-
-        for (i, x) in a {
-            *data.entry(i).or_default() += x;
-        }
-
-        Self { data }
+    pub fn from_vec(mut a: Vec<(usize, ConstModInt<P>)>) -> Self {
+        a.sort_by_key(|a| a.0);
+        Self { data: a }
     }
 
     /// $x^i$の係数を得る。
     pub fn coeff_of(&self, i: usize) -> ConstModInt<P> {
-        self.data.get(&i).map_or(0.into(), |a| *a)
+        self.data
+            .iter()
+            .find(|(j, _)| *j == i)
+            .map_or(0.into(), |(_, x)| *x)
     }
 
-    // pub fn differential(&mut self) {
+    pub fn differential(&mut self) {
+        let a = self
+            .data
+            .iter()
+            .filter_map(|&(i, x)| {
+                if i == 0 {
+                    None
+                } else {
+                    Some((i - 1, x * i.into()))
+                }
+            })
+            .collect();
 
-    // }
+        self.data = a;
+    }
+
+    pub fn integral(&mut self) {
+        let a = self
+            .data
+            .iter()
+            .map(|&(i, x)| (i + 1, x * ConstModInt::new(i as u32 + 1).inv()))
+            .collect();
+
+        self.data = a;
+    }
 }
