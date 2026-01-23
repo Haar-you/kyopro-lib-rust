9f365d099d5c49c674ac5f58c42a52b6fafa336d
Haar-you <zemlya.pole@gmail.com>
2026-01-23 23:51:20 +0900

    [update] Field::div

--- src/algebra/semiring/mod.rs
+++ src/algebra/semiring/mod.rs
@@ -14,17 +14,17 @@ pub trait Semiring {
     fn zero(&self) -> Self::Element;
     /// 乗法の単位元
     fn one(&self) -> Self::Element;
-    /// 加法(可換で結合的な二項演算)
+    /// 加法$\oplus$
     fn add(&self, a: Self::Element, b: Self::Element) -> Self::Element;
-    /// 乗法(結合的な二項演算)
+    /// 乗法$\otimes$
     fn mul(&self, a: Self::Element, b: Self::Element) -> Self::Element;
 }
 
 /// 環
 pub trait Ring: Semiring {
-    /// 加法の逆元
+    /// 加法の逆元 $-a$
     fn neg(&self, a: Self::Element) -> Self::Element;
-    /// 減法
+    /// $a \oplus (-b)$
     fn sub(&self, a: Self::Element, b: Self::Element) -> Self::Element {
         self.add(a, self.neg(b))
     }
@@ -32,6 +32,10 @@ pub trait Ring: Semiring {
 
 /// 体
 pub trait Field: Ring {
-    /// 乗法の逆元
+    /// 乗法の逆元 $a^{-1}$
     fn inv(&self, a: Self::Element) -> Self::Element;
+    /// $a \otimes b^{-1}$
+    fn div(&self, a: Self::Element, b: Self::Element) -> Self::Element {
+        self.mul(a, self.inv(b))
+    }
 }
